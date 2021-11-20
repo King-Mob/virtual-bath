ca198c1c01df452c2eab311e12227bf590925bb1
KingMob <john@onethreethree.net>
2021-11-20 23:15:51 +0000

    add bath temperature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 const App = () => {
   const { client } = useContext(MatrixContext);
+  const [loadingMessage, setLoadingMessage] = useState("connecting to bath...");
   const [waterVolume, setWaterVolume] = useState(50);
   const [waterTemp, setWaterTemp] = useState(0);
   const [coldTap, setColdTap] = useState();
@@ -100,6 +101,8 @@ const App = () => {
         }
       );
 
+      setLoadingMessage("replaying past bath events...");
+
       client.on("Room.timeline", handleEvent);
 
       let waterVolume = 0;
@@ -110,13 +113,28 @@ const App = () => {
       let hotFlow = 0;
       let plugged = true;
 
-      firstBath.timeline.forEach((item) => {
-        const eventType = item.event.type;
+      firstBath.timeline.forEach((item, index) => {
+        setLoadingMessage(
+          `replaying past bath events...${index}/${firstBath.timeline.length}`
+        );
+        console.log(waterTemp);
 
         time = (item.event.origin_server_ts - previousEventTime) / 1000;
 
         const potentialWaterVolume =
           waterVolume + (time * (coldFlow + hotFlow - (plugged ? 0 : 20))) / 10;
+
+        const currentFlowTemp =
+          coldFlow + hotFlow != 0
+            ? (coldFlow * 10 + hotFlow * 40) / (coldFlow + hotFlow)
+            : waterTemp;
+
+        waterTemp =
+          waterVolume + potentialWaterVolume != 0
+            ? (waterTemp * waterVolume +
+                (potentialWaterVolume - waterVolume) * currentFlowTemp) /
+              (waterVolume + (potentialWaterVolume - waterVolume))
+            : 0;
 
         if (potentialWaterVolume < 0) {
           waterVolume = 0;
@@ -125,6 +143,8 @@ const App = () => {
         } else {
           waterVolume = potentialWaterVolume;
         }
+
+        const eventType = item.event.type;
 
         switch (eventType) {
           case "bath.create":
@@ -160,6 +180,7 @@ const App = () => {
       //at the end, set the waterVolume and waterTemp for the current bath
       console.log(waterVolume);
       setWaterVolume(waterVolume);
+      setWaterTemp(waterTemp);
 
       setBathName(firstBath.name);
 
@@ -184,6 +205,21 @@ const App = () => {
       const potentialWaterVolume =
         waterVolume + (coldTap.flow + hotTap.flow - (plugged ? 0 : 20)) / 100;
 
+      const oldTemp = isNaN(waterTemp) ? 0 : waterTemp;
+
+      const currentFlowTemp =
+        coldTap.flow + hotTap.flow != 0
+          ? (coldTap.flow * 10 + hotTap.flow * 40) /
+            (coldTap.flow + hotTap.flow)
+          : oldTemp;
+
+      const newWater = Math.abs(potentialWaterVolume - waterVolume);
+
+      setWaterTemp(
+        (oldTemp * waterVolume + newWater * currentFlowTemp) /
+          (waterVolume + newWater)
+      );
+
       if (potentialWaterVolume < 0) {
         setWaterVolume(0);
       } else if (potentialWaterVolume > 100) {
@@ -194,17 +230,34 @@ const App = () => {
     }
   }, [counter]);
 
+  const calcWaterColour = (waterTemp) => {
+    const heatScalar = (waterTemp - 10) / 30;
+
+    const red = 109 + heatScalar * 91;
+    const blue = 216 + heatScalar * 25;
+
+    return red + "," + blue;
+  };
+
   return (
     <div className="bath-container">
-      <h1>bath time</h1>
+      <h1>Virtual Bath</h1>
+      <p className="tag-text">The world's leading virtual bathing experience</p>
       {bathLoaded ? (
         <div className="tub-container">
-          here's the bath <p>{bathName}</p>
+          <p>
+            {bathName} is {Math.round(waterVolume)}% full and{" "}
+            {Math.round(waterTemp)}°c
+          </p>
           <div className="taps-container">
             {coldTap && hotTap && (
               <>
-                <button onClick={() => toggleColdTap()}>Cold tap</button>
-                <button onClick={() => toggleHotTap()}>Hot tap</button>
+                <button onClick={() => toggleColdTap()}>
+                  Cold tap {coldTap.temp}°c
+                </button>
+                <button onClick={() => toggleHotTap()}>
+                  Hot tap {hotTap.temp}°c
+                </button>
               </>
             )}
           </div>
@@ -221,7 +274,7 @@ const App = () => {
             style={{
               backgroundImage: `linear-gradient(
                 to top,
-                rgba(160, 231, 255, 1) ${waterVolume}%,
+                rgba(${calcWaterColour(waterTemp)}, 255, 1) ${waterVolume}%,
                 rgba(0, 0, 0, 0) 1px,
                 rgba(0, 0, 0, 0)
               )`,
@@ -235,12 +288,16 @@ const App = () => {
               backgroundImage:
                 waterVolume == 0
                   ? `linear-gradient(rgba(255, 214, 239, 1), rgba(0, 0, 0, 0))`
-                  : `linear-gradient(rgba(160, 231, 255, 1), rgba(0, 0, 0, 0))`,
+                  : `linear-gradient(rgba(${calcWaterColour(
+                      waterTemp
+                    )}, 255, 1), rgba(0, 0, 0, 0))`,
             }}
           ></div>
         </div>
       ) : (
-        <p>loading bath...</p>
+        <div className="loading-bath-container">
+          <p>{loadingMessage}</p>
+        </div>
       )}
     </div>
   );
