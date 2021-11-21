3c127ee88b9f1f11d8bd33d6fcc73d41c8f2f9fa
KingMob <john@onethreethree.net>
2021-11-21 01:11:38 +0000

    add new bath creation

---
+++ src/components/Bath.js
@@ -0,0 +1,312 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { MatrixContext } from "../context/MatrixContext";
+import "../App.css";
+import NewBathMenu from "./NewBathMenu";
+
+const Bath = ({ bathId }) => {
+  const { client } = useContext(MatrixContext);
+  const [loadingMessage, setLoadingMessage] = useState("connecting to bath...");
+  const [waterVolume, setWaterVolume] = useState(50);
+  const [waterTemp, setWaterTemp] = useState(0);
+  const [coldTap, setColdTap] = useState();
+  const [hotTap, setHotTap] = useState();
+  const [plugged, setPlugged] = useState(true);
+  const [bathLoaded, setBathLoaded] = useState(false);
+  const [bathName, setBathName] = useState();
+  const [counter, setCounter] = useState(0);
+
+  if (!bathId) {
+    let { bath } = useParams();
+    bathId = bath;
+  }
+
+  const toggleColdTap = () => {
+    if (coldTap.flow == 0) {
+      client.sendEvent(bathId, "bath.tap.turn", {
+        temp: coldTap.temp,
+        flow: 10,
+      });
+    } else {
+      client.sendEvent(bathId, "bath.tap.turn", {
+        temp: coldTap.temp,
+        flow: 0,
+      });
+    }
+  };
+
+  const toggleHotTap = () => {
+    if (hotTap.flow == 0) {
+      client.sendEvent(bathId, "bath.tap.turn", {
+        temp: hotTap.temp,
+        flow: 10,
+      });
+    } else {
+      client.sendEvent(bathId, "bath.tap.turn", {
+        temp: hotTap.temp,
+        flow: 0,
+      });
+    }
+  };
+
+  const togglePlug = () => {
+    if (plugged) {
+      client.sendEvent(bathId, "bath.plug.pull", {});
+    } else {
+      client.sendEvent(bathId, "bath.plug.push", {});
+    }
+  };
+
+  const handleEvent = (event) => {
+    //change taps and plug as events come in
+    console.log(event);
+
+    const eventType = event.event.type;
+
+    switch (eventType) {
+      case "bath.tap.turn":
+        if (event.event.content.temp == hotTap.temp) {
+          setHotTap({
+            flow: event.event.content.flow,
+            temp: hotTap.temp,
+          });
+        } else {
+          setColdTap({
+            flow: event.event.content.flow,
+            temp: coldTap.temp,
+          });
+        }
+        break;
+      case "bath.plug.pull":
+        setPlugged(false);
+        break;
+      case "bath.plug.push":
+        setPlugged(true);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const initialiseBath = async () => {
+    if (client) {
+      const firstBath = await client.getRoom(bathId);
+      await client.paginateEventTimeline(
+        firstBath.timelineSets[0].liveTimeline,
+        {
+          backwards: true,
+          limit: 100000,
+        }
+      );
+
+      setLoadingMessage("replaying past bath events...");
+
+      let waterVolume = 0;
+      let waterTemp = 0;
+      let time = 0;
+      let previousEventTime = 0;
+      let coldFlow = 0;
+      let hotFlow = 0;
+      let coldTemp = 10;
+      let hotTemp = 40;
+      let plugged = true;
+
+      await firstBath.timeline.forEach((item, index) => {
+        setLoadingMessage(
+          `replaying past bath events...${index}/${firstBath.timeline.length}`
+        );
+        console.log(waterTemp);
+
+        time = (item.event.origin_server_ts - previousEventTime) / 1000;
+
+        const potentialWaterVolume =
+          waterVolume + (time * (coldFlow + hotFlow - (plugged ? 0 : 20))) / 10;
+
+        const currentFlowTemp =
+          coldFlow + hotFlow != 0
+            ? (coldFlow * coldTemp + hotFlow * hotTemp) / (coldFlow + hotFlow)
+            : waterTemp;
+
+        waterTemp =
+          waterVolume + potentialWaterVolume != 0
+            ? (waterTemp * waterVolume +
+                (potentialWaterVolume - waterVolume) * currentFlowTemp) /
+              (waterVolume + (potentialWaterVolume - waterVolume))
+            : 0;
+
+        if (potentialWaterVolume < 0) {
+          waterVolume = 0;
+        } else if (potentialWaterVolume > 100) {
+          waterVolume = 100;
+        } else {
+          waterVolume = potentialWaterVolume;
+        }
+
+        const eventType = item.event.type;
+
+        switch (eventType) {
+          case "bath.create":
+            coldTemp = item.event.content.taps[0];
+            setColdTap({
+              flow: 0,
+              temp: item.event.content.taps[0],
+            });
+            hotTemp = item.event.content.taps[1];
+            setHotTap({
+              flow: 0,
+              temp: item.event.content.taps[1],
+            });
+            break;
+          case "bath.tap.turn":
+            if (item.event.content.temp == 40) {
+              hotFlow = item.event.content.flow;
+            } else {
+              coldFlow = item.event.content.flow;
+            }
+            break;
+          case "bath.plug.pull":
+            plugged = false;
+            break;
+          case "bath.plug.push":
+            plugged = true;
+            break;
+          default:
+            break;
+        }
+
+        previousEventTime = item.event.origin_server_ts; //time is in milliseconds
+      });
+
+      //at the end, set the waterVolume and waterTemp for the current bath
+      console.log(waterVolume);
+      setWaterVolume(waterVolume);
+      setWaterTemp(waterTemp);
+
+      setBathName(firstBath.name);
+
+      setBathLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    initialiseBath();
+  }, [client]);
+
+  useEffect(() => {
+    if (coldTap && hotTap) {
+      client.on("Room.timeline", handleEvent);
+    }
+  }, [coldTap, hotTap]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(counter + 1);
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [counter]);
+
+  useEffect(() => {
+    if (coldTap && hotTap) {
+      const potentialWaterVolume =
+        waterVolume + (coldTap.flow + hotTap.flow - (plugged ? 0 : 20)) / 100;
+
+      const oldTemp = isNaN(waterTemp) ? 0 : waterTemp;
+
+      const currentFlowTemp =
+        coldTap.flow + hotTap.flow != 0
+          ? (coldTap.flow * coldTap.temp + hotTap.flow * hotTap.temp) /
+            (coldTap.flow + hotTap.flow)
+          : oldTemp;
+
+      const newWater = Math.abs(potentialWaterVolume - waterVolume);
+
+      setWaterTemp(
+        (oldTemp * waterVolume + newWater * currentFlowTemp) /
+          (waterVolume + newWater)
+      );
+
+      if (potentialWaterVolume < 0) {
+        setWaterVolume(0);
+      } else if (potentialWaterVolume > 100) {
+        setWaterVolume(100);
+      } else {
+        setWaterVolume(potentialWaterVolume);
+      }
+    }
+  }, [counter]);
+
+  const calcWaterColour = (waterTemp) => {
+    const tapHeatDifference = hotTap.temp - coldTap.temp;
+    const heatScalar = (waterTemp - coldTap.temp) / tapHeatDifference;
+
+    const red = 109 + heatScalar * 91;
+    const blue = 216 + heatScalar * 25;
+
+    return red + "," + blue;
+  };
+
+  return (
+    <div className="bath-container">
+      <NewBathMenu />
+      {bathLoaded ? (
+        <div className="tub-container">
+          <p>
+            {bathName} is {Math.round(waterVolume)}% full and{" "}
+            {Math.round(waterTemp)}°c
+          </p>
+          <div className="taps-container">
+            {coldTap && hotTap && (
+              <>
+                <button onClick={() => toggleColdTap()}>
+                  Cold tap {coldTap.temp}°c
+                </button>
+                <button onClick={() => toggleHotTap()}>
+                  Hot tap {hotTap.temp}°c
+                </button>
+              </>
+            )}
+          </div>
+          <div className="stream-container">
+            {coldTap && hotTap && (
+              <>
+                {coldTap.flow > 0 ? <div className="stream cold"></div> : null}
+                {hotTap.flow > 0 ? <div className="stream hot"></div> : null}
+              </>
+            )}
+          </div>
+          <div
+            className="tub"
+            style={{
+              backgroundImage: `linear-gradient(
+                to top,
+                rgba(${calcWaterColour(waterTemp)}, 255, 1) ${waterVolume}%,
+                rgba(0, 0, 0, 0) 1px,
+                rgba(0, 0, 0, 0)
+              )`,
+            }}
+          ></div>
+          <button onClick={togglePlug}>Plug</button>
+          <div
+            className="drain"
+            style={{
+              display: plugged ? "none" : "block",
+              backgroundImage:
+                waterVolume == 0
+                  ? `linear-gradient(rgba(255, 214, 239, 1), rgba(0, 0, 0, 0))`
+                  : `linear-gradient(rgba(${calcWaterColour(
+                      waterTemp
+                    )}, 255, 1), rgba(0, 0, 0, 0))`,
+            }}
+          ></div>
+        </div>
+      ) : (
+        <div className="loading-bath-container">
+          <p>{loadingMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Bath;
---
+++ src/components/NewBathMenu.js
@@ -0,0 +1,86 @@
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { MatrixContext } from "../context/MatrixContext";
+import "../App.css";
+
+const NewBathMenu = () => {
+  const { client } = useContext(MatrixContext);
+  const [newBathMenuVisible, setNewBathMenuVisible] = useState(false);
+  const [newBathName, setNewBathName] = useState("");
+  const [newColdTemp, setNewColdTemp] = useState(10);
+  const [newHotTemp, setNewHotTemp] = useState(40);
+  const [newBathId, setNewBathId] = useState();
+
+  const createBath = async () => {
+    if (newBathName.length > 0) {
+      const newBath = await client.createRoom({
+        visibility: "private",
+        name: newBathName,
+      });
+
+      await client.sendEvent(newBath.room_id, "bath.create", {
+        taps: [newColdTemp, newHotTemp],
+      });
+
+      setNewBathId(newBath.room_id);
+    }
+  };
+
+  return (
+    <div>
+      {newBathMenuVisible ? (
+        <div className="new-bath-menu-container">
+          {newBathId ? (
+            <Link to={`../private/${newBathId}`}>Go to {newBathName}</Link>
+          ) : (
+            <>
+              <div className="new-bath-menu-item">
+                <label for="bathName">Name</label>
+                <input
+                  type="text"
+                  value={newBathName}
+                  onChange={(e) => setNewBathName(e.target.value)}
+                  placeholder="new bath name"
+                  name="bathName"
+                ></input>
+              </div>
+              <div className="new-bath-menu-item">
+                <label for="coldtemp">Cold temp</label>
+                <input
+                  type="number"
+                  value={newColdTemp}
+                  onChange={(e) => setNewColdTemp(e.target.value)}
+                  placeholder="temp in °c"
+                  name="coldTemp"
+                ></input>
+              </div>
+              <div className="new-bath-menu-item">
+                <label for="hotTemp">Hot temp</label>
+                <input
+                  type="number"
+                  value={newHotTemp}
+                  onChange={(e) => setNewHotTemp(e.target.value)}
+                  placeholder="temp in °c"
+                  name="hotTemp"
+                ></input>
+              </div>
+              <button onClick={() => setNewBathMenuVisible(false)}>
+                cancel
+              </button>
+              <button onClick={createBath}>Create Bath</button>
+            </>
+          )}
+        </div>
+      ) : (
+        <button
+          onClick={() => setNewBathMenuVisible(true)}
+          className="new-bath-menu-button"
+        >
+          create new bath
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default NewBathMenu;
